47ddc79d30a326ac9eca9b67d02e9ee44a5e09ba
Kamil Bobrowski <kamil.bobrowski@gmail.com>
2020-06-03 16:02:09 +0600

    added licensing comment

--- src/main/java/dev/fastgql/common/QualifiedName.java
+++ src/main/java/dev/fastgql/common/QualifiedName.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright fastGQL Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
 package dev.fastgql.common;
 
 import org.antlr.v4.runtime.misc.Pair;
--- src/main/java/dev/fastgql/graphql/GraphQLDatabaseSchema.java
+++ src/main/java/dev/fastgql/graphql/GraphQLDatabaseSchema.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright fastGQL Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
 package dev.fastgql.graphql;
 
 import dev.fastgql.db.DatabaseSchema;
