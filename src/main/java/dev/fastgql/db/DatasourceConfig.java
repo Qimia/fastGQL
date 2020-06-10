e2a2943d901ae861527a8b8956931000eb49ba23
Mingyi Zhang <mingyizhang1987@gmail.com>
2020-06-10 20:49:42 +0200

    Prepare for merge from master

--- src/main/java/dev/fastgql/db/DatasourceConfig.java
+++ src/main/java/dev/fastgql/db/DatasourceConfig.java
@@ -7,6 +7,18 @@
 package dev.fastgql.db;
 
 import io.vertx.core.json.JsonObject;
+import io.vertx.mysqlclient.MySQLConnectOptions;
+import io.vertx.pgclient.PgConnectOptions;
+import io.vertx.reactivex.core.Vertx;
+import io.vertx.reactivex.mysqlclient.MySQLPool;
+import io.vertx.reactivex.pgclient.PgPool;
+import io.vertx.reactivex.sqlclient.Pool;
+import io.vertx.sqlclient.PoolOptions;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Datasource config.
@@ -14,44 +26,160 @@ import io.vertx.core.json.JsonObject;
  * @author Kamil Bobrowski
  */
 public class DatasourceConfig {
+  private enum DBType {
+    postgresql,
+    mysql,
+    other
+  }
+
+  private final String jdbcUrl;
+  private final DBType dbType;
   private final String host;
   private final int port;
   private final String db;
   private final String username;
   private final String password;
+  private final String schema;
+
+  private DatasourceConfig(
+      String jdbcUrl,
+      DBType dbType,
+      String host,
+      int port,
+      String db,
+      String schema,
+      String username,
+      String password) {
+    this.jdbcUrl = jdbcUrl;
+    this.dbType = dbType;
+    this.host = host;
+    this.port = port;
+    this.db = db;
+    this.schema = schema;
+    this.username = username;
+    this.password = password;
+  }
+
+  private static class JdbcParseResult {
+    private final DBType dbType;
+    private final String host;
+    private final int port;
+    private final String db;
+
+    private JdbcParseResult(DBType dbType, String host, int port, String db) {
+      this.dbType = dbType;
+      this.host = host;
+      this.port = port;
+      this.db = db;
+    }
+
+    private static JdbcParseResult parse(String jdbcUrl) {
+      Pattern pattern =
+          Pattern.compile("jdbc:(postgresql|mysql)://(\\w+):(\\d{4,5})/(\\w+)\\??\\S*");
+      Matcher matcher = pattern.matcher(jdbcUrl);
+      if (matcher.matches()) {
+        String dbTypeString = matcher.group(1);
+        DBType dbType = DBType.other;
+        if (dbTypeString.equals("postgresql")) {
+          dbType = DBType.postgresql;
+        } else if (dbTypeString.equals("mysql")) {
+          dbType = DBType.mysql;
+        }
+        if (dbType == DBType.other) {
+          throw new IllegalArgumentException("Unsupported DB type");
+        }
+        String host = matcher.group(2);
+        int port = Integer.parseInt(matcher.group(3));
+        String db = matcher.group(4);
+        return new JdbcParseResult(dbType, host, port, db);
+      } else {
+        throw new IllegalArgumentException("JDBC url not valid");
+      }
+    }
+  }
+
+  public Connection getConnection() throws SQLException {
+    return DriverManager.getConnection(jdbcUrl, username, password);
+  }
+
+  /**
+   * Generate {@link Pool} of proper type.
+   *
+   * @param vertx vertx instance
+   * @return new Pool
+   */
+  public Pool getPool(Vertx vertx) {
+    switch (dbType) {
+      case mysql:
+        return MySQLPool.pool(
+            vertx,
+            new MySQLConnectOptions()
+                .setHost(host)
+                .setPort(port)
+                .setDatabase(db)
+                .setUser(username)
+                .setPassword(password),
+            new PoolOptions().setMaxSize(5));
+      case postgresql:
+        return PgPool.pool(
+            vertx,
+            new PgConnectOptions()
+                .setHost(host)
+                .setPort(port)
+                .setDatabase(db)
+                .setUser(username)
+                .setPassword(password),
+            new PoolOptions().setMaxSize(5));
+      case other:
+      default:
+        throw new RuntimeException("Cannot generate Pool for unsupported database type");
+    }
+  }
+
+  /**
+   * Standard create method.
+   *
+   * @param jdbcUrl JDBC url
+   * @param username database user name
+   * @param password database password
+   */
+  public static DatasourceConfig createDatasourceConfig(
+      String jdbcUrl, String username, String password) {
+    JdbcParseResult result = JdbcParseResult.parse(jdbcUrl);
+    return new DatasourceConfig(
+        jdbcUrl, result.dbType, result.host, result.port, result.db, result.db, username, password);
+  }
+
+  /**
+   * Create method with custom schema.
+   *
+   * @param jdbcUrl JDBC url
+   * @param username database user name
+   * @param password database password
+   * @param schema database schema
+   */
+  public static DatasourceConfig createDatasourceConfig(
+      String jdbcUrl, String username, String password, String schema) {
+    JdbcParseResult result = JdbcParseResult.parse(jdbcUrl);
+    return new DatasourceConfig(
+        jdbcUrl, result.dbType, result.host, result.port, result.db, schema, username, password);
+  }
 
   /**
    * Create datasource from json config.
    *
    * @param config json config
    */
-  public DatasourceConfig(JsonObject config) {
-    this.host = config.getString("host");
-    this.port = config.getInteger("port");
-    this.db = config.getString("db");
-    this.username = config.getString("username");
-    this.password = config.getString("password");
-  }
-
-  /**
-   * Standard constructor.
-   *
-   * @param host host (e.g. "localhost")
-   * @param port database port (e.g. 5432)
-   * @param db database name
-   * @param username database user name
-   * @param password database password
-   */
-  public DatasourceConfig(String host, int port, String db, String username, String password) {
-    this.host = host;
-    this.port = port;
-    this.db = db;
-    this.username = username;
-    this.password = password;
+  public static DatasourceConfig createWithJsonConfig(JsonObject config) {
+    return createDatasourceConfig(
+        config.getString("jdbcUrl"),
+        config.getString("username"),
+        config.getString("password"),
+        config.getString("schema"));
   }
 
   public String getJdbcUrl() {
-    return String.format("jdbc:postgresql://%s:%d/%s", host, port, db);
+    return jdbcUrl;
   }
 
   public String getHost() {
@@ -64,6 +192,10 @@ public class DatasourceConfig {
 
   public String getDb() {
     return db;
+  }
+
+  public String getSchema() {
+    return schema;
   }
 
   public String getUsername() {
