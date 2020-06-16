b479c92d13e6701ecc3066719c6c8433ceb4eaf3
Mingyi Zhang <mingyizhang1987@gmail.com>
2020-06-16 13:33:12 +0200

    Refactor KeyDefinitionTest

--- src/test/java/dev/fastgql/db/KeyDefinitionTest.java
+++ src/test/java/dev/fastgql/db/KeyDefinitionTest.java
@@ -5,118 +5,122 @@
  */
 
 package dev.fastgql.db;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import dev.fastgql.common.KeyType;
 import dev.fastgql.common.QualifiedName;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class KeyDefinitionTest {
 
-    KeyDefinition thisKeyDefinition;
+  KeyDefinition keyDefinition;
 
-    @BeforeEach
-    public void setUp() {
-        thisKeyDefinition = new KeyDefinition(
-                new QualifiedName("tableName1", "keyName1"),
-                KeyType.STRING,
-                new QualifiedName("tableName2", "keyName2"),
-                new HashSet<>(Arrays.asList(new QualifiedName("tableName3", "keyName3"),
-                        new QualifiedName("tableName4", "keyName4"))));
-    }
+  @BeforeEach
+  public void setUp() {
+    keyDefinition =
+        new KeyDefinition(
+            new QualifiedName("tableName1", "keyName1"),
+            KeyType.STRING,
+            new QualifiedName("tableName2", "keyName2"),
+            new HashSet<>(
+                Arrays.asList(
+                    new QualifiedName("tableName3", "keyName3"),
+                    new QualifiedName("tableName4", "keyName4"))));
+  }
 
-    @Test
-    public void shouldMergeEqualKeyDefinitions() {
-        Set<QualifiedName> thisReferences = thisKeyDefinition.getReferencedBy();
-        thisKeyDefinition.merge(thisKeyDefinition);
+  @Test
+  public void shouldMergeEqualKeyDefinitions() {
+    Set<QualifiedName> thisReferences = keyDefinition.getReferencedBy();
+    keyDefinition.merge(keyDefinition);
 
-        assertTrue(thisReferences.containsAll(thisKeyDefinition.getReferencedBy()));
-    }
+    assertTrue(thisReferences.containsAll(keyDefinition.getReferencedBy()));
+  }
 
-    @Test
-    public void throwsRuntimeExceptionWhileMergingWithNull() {
-        Exception exception = assertThrows(RuntimeException.class, () -> {
-            thisKeyDefinition.merge(null);
-        });
+  @Test
+  public void throwsRuntimeExceptionWhileMergingWithNull() {
+    Exception exception =
+        assertThrows(
+            RuntimeException.class,
+            () -> {
+              keyDefinition.merge(null);
+            });
 
-        String expectedMessage = "cannot merge with null key";
-        String actualMessage = exception.getMessage();
+    String expectedMessage = "cannot merge with null key";
+    String actualMessage = exception.getMessage();
 
-        assertTrue(actualMessage.contains(expectedMessage));
-    }
+    assertTrue(actualMessage.contains(expectedMessage));
+  }
 
-    @Test
-    public void throwsRuntimeExceptionForIncompatibleKeys() {
-        KeyDefinition otherKeyDefinition = new KeyDefinition(
-                new QualifiedName("tableName1x", "keyName1x"),
-                KeyType.STRING,
-                new QualifiedName("tableName2x", "keyName2x"),
-                new HashSet<>(Arrays.asList(new QualifiedName("tableName6", "keyName6"))));
+  @Test
+  public void throwsRuntimeExceptionForIncompatibleKeys() {
+    KeyDefinition otherKeyDefinition =
+        new KeyDefinition(
+            new QualifiedName("tableName1x", "keyName1x"),
+            KeyType.STRING,
+            new QualifiedName("tableName2x", "keyName2x"),
+            new HashSet<>(Collections.singletonList(new QualifiedName("tableName6", "keyName6"))));
+    assertThrows(
+        RuntimeException.class,
+        () -> keyDefinition.merge(otherKeyDefinition),
+        "keys to be merged not compatible");
+  }
 
-        Exception exception = assertThrows(RuntimeException.class, () -> {
-            thisKeyDefinition.merge(otherKeyDefinition);
-        });
+  @Test
+  public void throwsRuntimeExceptionForDifferentReferencings() {
+    KeyDefinition otherKeyDefinition =
+        new KeyDefinition(
+            new QualifiedName("tableName1", "keyName1"),
+            KeyType.STRING,
+            new QualifiedName("tableName2x", "keyName2x"),
+            new HashSet<>(Collections.singletonList(new QualifiedName("tableName6", "keyName6"))));
+    assertThrows(
+        RuntimeException.class,
+        () -> keyDefinition.merge(otherKeyDefinition),
+        "key is already referencing other key");
+  }
 
-        String expectedMessage = "keys to be merged not compatible";
-        String actualMessage = exception.getMessage();
+  @Test
+  public void shouldMergeKeyDefinitionsWithDifferentReferences() {
+    KeyDefinition otherKeyDefinition =
+        new KeyDefinition(
+            new QualifiedName("tableName1", "keyName1"),
+            KeyType.STRING,
+            new QualifiedName("tableName2", "keyName2"),
+            new HashSet<>(Collections.singletonList(new QualifiedName("tableName6", "keyName6"))));
 
-        assertTrue(actualMessage.contains(expectedMessage));
-    }
+    // saving both sets of references in order to generate the expected set
+    Set<QualifiedName> thisReferences = keyDefinition.getReferencedBy();
+    Set<QualifiedName> otherReferences = otherKeyDefinition.getReferencedBy();
+    thisReferences.addAll(otherReferences);
 
-    @Test
-    public void throwsRuntimeExceptionForDifferentReferencings() {
-        KeyDefinition otherKeyDefinition = new KeyDefinition(
-                new QualifiedName("tableName1", "keyName1"),
-                KeyType.STRING,
-                new QualifiedName("tableName2x", "keyName2x"),
-                new HashSet<>(Arrays.asList(new QualifiedName("tableName6", "keyName6"))));
+    // merging of KeyDefinitions
+    keyDefinition.merge(otherKeyDefinition);
 
-        Exception exception = assertThrows(RuntimeException.class, () -> {
-            thisKeyDefinition.merge(otherKeyDefinition);
-        });
+    assertTrue(keyDefinition.getReferencedBy().containsAll(thisReferences));
+  }
 
-        String expectedMessage = "key is already referencing other key";
-        String actualMessage = exception.getMessage();
+  @Test
+  public void shouldMergeKeyDefinitionsWithNullOtherReferences() {
+    KeyDefinition otherKeyDefinition =
+        new KeyDefinition(
+            new QualifiedName("tableName1", "keyName1"),
+            KeyType.STRING,
+            new QualifiedName("tableName2", "keyName2"),
+            null);
 
-        assertTrue(actualMessage.contains(expectedMessage));
-    }
+    // saving the set of 'this' references as the expected set
+    Set<QualifiedName> thisReferences = keyDefinition.getReferencedBy();
 
-    @Test
-    public void shouldMergeKeyDefinitionsWithDifferentReferences() {
-        KeyDefinition otherKeyDefinition = new KeyDefinition(
-                new QualifiedName("tableName1", "keyName1"),
-                KeyType.STRING,
-                new QualifiedName("tableName2", "keyName2"),
-                new HashSet<>(Arrays.asList(new QualifiedName("tableName6", "keyName6"))));
+    // merging of KeyDefinitions
+    keyDefinition.merge(otherKeyDefinition);
 
-        // saving both sets of references in order to generate the expected set
-        Set<QualifiedName> thisReferences = thisKeyDefinition.getReferencedBy();
-        Set<QualifiedName> otherReferences = otherKeyDefinition.getReferencedBy();
-        thisReferences.addAll(otherReferences);
-
-        // merging of KeyDefinitions
-        thisKeyDefinition.merge(otherKeyDefinition);
-
-        assertTrue(thisKeyDefinition.getReferencedBy().containsAll(thisReferences));
-    }
-
-    @Test
-    public void shouldMergeKeyDefinitionsWithNullOtherReferences() {
-        KeyDefinition otherKeyDefinition = new KeyDefinition(
-                new QualifiedName("tableName1", "keyName1"),
-                KeyType.STRING,
-                new QualifiedName("tableName2", "keyName2"),
-                null);
-
-        // saving the set of 'this' references as the expected set
-        Set<QualifiedName> thisReferences = thisKeyDefinition.getReferencedBy();
-
-        // merging of KeyDefinitions
-        thisKeyDefinition.merge(otherKeyDefinition);
-
-        assertTrue(thisKeyDefinition.getReferencedBy().containsAll(thisReferences));
-    }
+    assertTrue(keyDefinition.getReferencedBy().containsAll(thisReferences));
+  }
 }
