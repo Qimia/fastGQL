e501efbdb3cc769aa40a9a3052863fd183d19d2e
Kamil Bobrowski <kamil.bobrowski@gmail.com>
2020-08-05 17:00:17 +0200

    Eliminated delay from subscription testing (#66)

--- src/test/java/dev/fastgql/integration/SubscriptionTests.java
+++ src/test/java/dev/fastgql/integration/SubscriptionTests.java
@@ -1,13 +1,14 @@
 package dev.fastgql.integration;
 
+import io.vertx.core.http.HttpClientOptions;
 import io.vertx.junit5.VertxTestContext;
 import io.vertx.reactivex.core.Vertx;
+import io.vertx.reactivex.core.http.HttpClient;
 import io.vertx.reactivex.ext.web.client.WebClient;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.sql.SQLException;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -36,25 +37,25 @@ public interface SubscriptionTests extends SetupTearDownForEach {
     }
 
     WebClient client = WebClient.create(vertx);
+    HttpClient httpClient =
+        vertx.createHttpClient(new HttpClientOptions().setDefaultPort(getDeploymentPort()));
+    String query = String.format("%s/query.graphql", directory);
+    List<String> expected =
+        List.of(
+            String.format("%s/expected-1.json", directory),
+            String.format("%s/expected-2.json", directory));
     client
         .get(getDeploymentPort(), "localhost", "/update")
         .rxSend()
+        .flatMap(response -> httpClient.rxWebSocket("/graphql"))
+        .flatMapCompletable(
+            webSocket ->
+                GraphQLTestUtils.startSubscription(query, context, webSocket)
+                    .andThen(GraphQLTestUtils.verifySubscription(expected, context, webSocket)))
         .subscribe(
-            response -> {
-              String query = String.format("%s/query.graphql", directory);
-              List<String> expected =
-                  List.of(
-                      String.format("%s/expected-1.json", directory),
-                      String.format("%s/expected-2.json", directory));
-              GraphQLTestUtils.verifySubscription(
-                  getDeploymentPort(), query, expected, vertx, context);
-              DBTestUtils.executeSQLQueryFromResourceWithDelay(
-                  String.format("%s/query.sql", directory),
-                  10,
-                  TimeUnit.SECONDS,
-                  getJdbcDatabaseContainer(),
-                  context);
-            },
+            () ->
+                DBTestUtils.executeSQLQueryFromResourceWithContext(
+                    String.format("%s/query.sql", directory), getJdbcDatabaseContainer(), context),
             context::failNow);
   }
 }
